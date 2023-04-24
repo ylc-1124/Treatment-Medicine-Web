809aaf231c3a4e8e90c24fc95b3ab010cd041fd9
ylc-1124 <86100547+ylc-1124@users.noreply.github.com>
2023-04-24 19:23:55 +0800

    Update FileServiceImpl.java

--- src/main/java/edu/sust/oss/service/impl/FileServiceImpl.java
+++ src/main/java/edu/sust/oss/service/impl/FileServiceImpl.java
@@ -23,8 +23,8 @@ public class FileServiceImpl implements FileService {
         // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
         String endpoint = "oss-cn-beijing.aliyuncs.com";
         // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5tNxW5uH8MDQh83dkhJe";
-        String accessKeySecret = "KTMTxyoSFB15Oh37bLWAOW2ZoEDYwY";
+        String accessKeyId = "";
+        String accessKeySecret = "";
         // 填写Bucket名称，例如examplebucket。
         String bucketName = "zjz-doctor";
         // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
